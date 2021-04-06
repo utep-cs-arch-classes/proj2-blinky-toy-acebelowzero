bb6adf6a7ae64da076ba18ca5e19fc6cb230d819
Michael <likemike9921@gmail.com>
2021-04-06 09:48:20 -0600

    moved files from blink_demo

--- project/switches.c
+++ project/switches.c
@@ -2,3 +2,11 @@
 #include "switches.h"
 #include "led.h"
 
+char state_down,  state_change;
+
+static char switch_update_interrupt_sense(){
+	char p2val = P2IN;
+	PEIES |= (p2val & SWITCHES);
+	P2IES &= (p2val & ~SWITCHES);
+	return p2val;
+}
