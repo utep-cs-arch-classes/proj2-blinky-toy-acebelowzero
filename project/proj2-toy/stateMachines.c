c21deccfcc97376903ae625c34d867e64ed5b58a
Michael <likemike9921@gmail.com>
2021-04-07 14:49:26 -0600

    added different states

--- project/proj2-toy/stateMachines.c
+++ project/proj2-toy/stateMachines.c
@@ -6,35 +6,35 @@
 
 
 
-// char toggle_red()		/* always toggle! */
-// {
-//   static char state = 0;
+char toggle_red()		/* always toggle! */
+{
+  static char state = 0;
 
-//   switch (state) {
-//   case 0:
-//     buzzer_set_period(6000);
-//     red_on = 1;
-//     state = 1;
-//     break;
-//   case 1:
-//     buzzer_set_period(500);
-//     red_on = 0;
-//     state = 0;
-//     break;
-//   }
-//   return 1;			/* always changes an led */
-// }
+  switch (state) {
+  case 0:
+    buzzer_set_period(6000);
+    red_on = 1;
+    state = 1;
+    break;
+  case 1:
+    buzzer_set_period(500);
+    red_on = 0;
+    state = 0;
+    break;
+  }
+  return 1;			/* always changes an led */
+}
 
-// char toggle_green()	/* only toggle green if red is on!  */
-// {
-//   char changed = 0;
-//   if (red_on) {
-//     buzzer_set_period(200);
-//     green_on ^= 1;
-//     changed = 1;
-//   }
-//   return changed;
-// }
+char toggle_green()	/* only toggle green if red is on!  */
+{
+  char changed = 0;
+  if (red_on) {
+    buzzer_set_period(200);
+    green_on ^= 1;
+    changed = 1;
+  }
+  return changed;
+}
 
 
 void buzzer_stuff() {
@@ -43,17 +43,17 @@ void buzzer_stuff() {
 
 
 
-// char state0() {
-//   char changed = 0;  
-//   static enum {R=0, G=1} color = G;
-//   switch (color) {
-//   case R: changed = toggle_red(); color = G; break;
-//   case G: changed = toggle_green(); color = R; break;
-//   }
+char state0() {
+  char changed = 0;  
+  static enum {R=0, G=1} color = G;
+  switch (color) {
+  case R: changed = toggle_red(); color = G; break;
+  case G: changed = toggle_green(); color = R; break;
+  }
 
-//   led_changed = changed;
-//   led_update();
-// }
+  led_changed = changed;
+  led_update();
+}
 
 void state_advance()		/* alternate between toggling red & green */
 {
@@ -66,17 +66,15 @@ void state_advance()		/* alternate between toggling red & green */
     // in state 0 by default
     case 0: 
       buzzer_set_period(0);
-      green_on = 1;
-      red_on = 1;
-      changed = 1;
+      state0();
       break;
       
     // when side button click jump to state 1 then return to state 0 when SW1 is up
     case 1:
-      buzzer_set_period(1000);
-      green_on = 0;
-      red_on = 1;
+      green_on = 1;
+      red_on = 0;
       changed = 1;
+      buzzer_set_period(1000);
       break;
 
     case 2:
